ff9ef2bd76d26f6d958f3df4354dd99849e3b253
shivam <shivamguglani306@gmail.com>
2020-05-01 15:04:32 +0530

    new

--- project1.js
+++ project1.js
@@ -12,9 +12,9 @@ Addbtn.addEventListener('click', function (e) {
     } else {
         notesobj = JSON.parse(notes);
     }
-    myobj={
-        title:AddTitle.value,
-        text:AddText.value
+    myobj = {
+        title: AddTitle.value,
+        text: AddText.value
     }
     notesobj.push(myobj);
     localStorage.setItem("notes", JSON.stringify(notesobj));
@@ -38,12 +38,14 @@ function showNotes() {
 
         html += `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
-        <div class="card-body">
+        <div class="card-body" id=${index+1000}>
             <h5 class="card-title">${element.title}</h5>
             <p class="card-text"> ${element.text}</p>
             <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
-        </div>
-    </div>`
+            <button id="${index+222}" onclick="Important(this.id)"  class="btn btn-primary btn2">Important</button>
+            </div>
+            </div>`
+        // 
 
 
     });
@@ -59,7 +61,31 @@ function showNotes() {
 
 
 }
+// Important
+function Important(index222, index1000) {
+    let btn = document.getElementById(index222);
 
+    let body = document.getElementById(index1000);
+    console.log(body);
+    btn.classList.remove("btn-primary");
+    // btnDelete.classList.remove("btn-primary");
+    btn.classList.add('btn-danger');
+    // btnDelete.classList.add('btn-danger');
+    console.log('removed');
+    // body.classList.add('bg-danger');
+    console.log('added');
+
+    console.log(btn);
+    // btn.classlist.remove('btn-primary')
+    // element.classList.remove("mystyle");
+
+}
+
+//background
+function background(index1000, index222) {
+
+
+}
 
 //function to delete a note
 function deleteNote(index) {
@@ -79,7 +105,9 @@ function deleteNote(index) {
 }
 let Addbtn1 = document.getElementById("my");
 Addbtn1.addEventListener('click', function (e) {
-    console.log("sam111")
+    console.log("sam111");
+    window.location.reload()
+    // window.Location.reload();
     localStorage.setItem("notes", JSON.stringify([]));
 
 
@@ -89,17 +117,16 @@ Addbtn1.addEventListener('click', function (e) {
 //search
 
 let search = document.getElementById('searchTxt');
-search.addEventListener("input", function(){
+search.addEventListener("input", function () {
 
     let inputVal = search.value.toLowerCase();
     console.log('Input event fired!', inputVal);
     let noteCards = document.getElementsByClassName('noteCard');
-    Array.from(noteCards).forEach(function(element){
+    Array.from(noteCards).forEach(function (element) {
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if(cardTxt.includes(inputVal)){
+        if (cardTxt.includes(inputVal)) {
             element.style.display = "block";
-        }
-        else{
+        } else {
             element.style.display = "none";
         }
         console.log(cardTxt);
